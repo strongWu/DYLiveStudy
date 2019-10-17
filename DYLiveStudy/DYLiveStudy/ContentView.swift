d761d9e1d9ca1d51c0e2356206f144e5ea4fb758
strongWu <624594256@qq.com>
2019-10-17 20:06:05 +0800

    初始化项目

---
+++ DYLiveStudy/DYLiveStudy/ContentView.swift
@@ -0,0 +1,21 @@
+ //
+//  ContentView.swift
+//  DYLiveStudy
+//
+//  Created by wugang on 2019/10/17.
+//  Copyright © 2019 wugang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
